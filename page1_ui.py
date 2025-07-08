593e4d22913ae8a855be7f092fe9805358528bff
skwo27 <165741912+skwo27@users.noreply.github.com>
2025-07-08 16:30:29 +0900

    feat: progressbar 구현

--- page1_ui.py
+++ page1_ui.py
@@ -15,14 +15,16 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFrame, QMainWindow, QMenuBar,
-    QSizePolicy, QStatusBar, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QGroupBox, QLabel,
+    QMainWindow, QMenuBar, QProgressBar, QSizePolicy,
+    QStatusBar, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(960, 500)
+        MainWindow.resize(937, 500)
+        MainWindow.setAcceptDrops(False)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.centralwidget.setStyleSheet(u"")
@@ -61,16 +63,85 @@ class Ui_MainWindow(object):
         self.NetworkContainer.setContentsMargins(0, 0, 0, 0)
         self.NetworkContent = QFrame(self.verticalLayoutWidget_3)
         self.NetworkContent.setObjectName(u"NetworkContent")
-        self.NetworkContent.setStyleSheet(u"border-image:url(\"images/image2.png\")")
         self.verticalLayout_2 = QVBoxLayout(self.NetworkContent)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.groupBox = QGroupBox(self.NetworkContent)
+        self.groupBox.setObjectName(u"groupBox")
+        self.WAN = QProgressBar(self.groupBox)
+        self.WAN.setObjectName(u"WAN")
+        self.WAN.setEnabled(True)
+        self.WAN.setGeometry(QRect(90, 70, 345, 10))
+        font = QFont()
+        font.setPointSize(1)
+        font.setWeight(QFont.Thin)
+        self.WAN.setFont(font)
+        self.WAN.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+        self.WAN.setStyleSheet(u"QProgressBar {\n"
+"    border-radius: 5px;\n"
+"    background-color: #ff5151;\n"
+"    text-align: center;\n"
+"    color: black;\n"
+"}\n"
+"\n"
+"QProgressBar::chunk {\n"
+"    background-color: #fff700;\n"
+"    border-top-left-radius: 5px;\n"
+"    border-bottom-left-radius: 5px;\n"
+"}")
+        self.WAN.setMaximum(0)
+        self.WAN.setValue(-1)
+        self.WAN.setTextVisible(False)
+        self.WAN.setInvertedAppearance(False)
+        self.label = QLabel(self.groupBox)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(40, 66, 41, 16))
+        font1 = QFont()
+        font1.setPointSize(14)
+        font1.setBold(True)
+        self.label.setFont(font1)
+        self.label.setStyleSheet(u"color: rgb(255, 255,255);")
+        self.label_2 = QLabel(self.groupBox)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(44, 96, 31, 16))
+        self.label_2.setFont(font1)
+        self.label_2.setStyleSheet(u"color: rgb(255, 255,255);")
+        self.LAN = QProgressBar(self.groupBox)
+        self.LAN.setObjectName(u"LAN")
+        self.LAN.setGeometry(QRect(90, 100, 200, 10))
+        self.LAN.setFont(font)
+        self.LAN.setStyleSheet(u"QProgressBar {\n"
+"    border-radius: 5px;\n"
+"    background-color: #ff5151;\n"
+"    text-align: center;\n"
+"    color: black;\n"
+"}\n"
+"\n"
+"QProgressBar::chunk {\n"
+"    background-color: #fff700;\n"
+"    border-top-left-radius: 5px;\n"
+"    border-bottom-left-radius: 5px;\n"
+"}")
+        self.LAN.setValue(45)
+        self.LAN.setTextVisible(False)
+        self.LAN.setInvertedAppearance(False)
+        self.line = QFrame(self.groupBox)
+        self.line.setObjectName(u"line")
+        self.line.setEnabled(False)
+        self.line.setGeometry(QRect(130, -10, 293, 20))
+        self.line.setStyleSheet(u"color: #a3a3a3;")
+        self.line.setFrameShadow(QFrame.Shadow.Plain)
+        self.line.setLineWidth(2)
+        self.line.setFrameShape(QFrame.Shape.HLine)
+
+        self.verticalLayout_2.addWidget(self.groupBox)
+
 
         self.NetworkContainer.addWidget(self.NetworkContent)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 960, 36))
+        self.menubar.setGeometry(QRect(0, 0, 937, 33))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -83,5 +154,9 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.NetworkContent.setStyleSheet("")
+        self.groupBox.setTitle("")
+        self.label.setText(QCoreApplication.translate("MainWindow", u"WAN", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"LAN", None))
     # retranslateUi
 
